f0dc0a9776051ea99b15f580f8380b68ebd3471f
Amit <amitbaderia@gmail.com>
2021-11-28 12:17:59 +0530

    card icon and cart drop down toggle functionality added

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,15 @@
+import CustomButton from '../custom-button/custom-button.component';
+import './cart-dropdown.styles.scss';
+
+import React from 'react';
+
+const CartDropDown = () => {
+	return (
+		<div className='cart-dropdown'>
+			<div className='cart-items'></div>
+			<CustomButton>GO TO CHECKOUT</CustomButton>
+		</div>
+	);
+};
+
+export default CartDropDown;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils.js';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
 	return (
 		<div className='header'>
 			<Link to='/' className='logo-container'>
@@ -28,13 +30,17 @@ const Header = ({ currentUser }) => {
 						SIGN IN
 					</Link>
 				)}
+
+				<CartIcon />
 			</div>
+			{hidden ? null : <CartDropDown />}
 		</div>
 	);
 };
 
-const mapToStateToProps = (state) => ({
-	currentUser: state.user.currentUser,
+const mapToStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden,
 });
 
 export default connect(mapToStateToProps)(Header);
